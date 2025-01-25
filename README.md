15dc9676f33684e5d39ef054c0b766e55d1a7889
Elena-Anishch <elena.anishchenko@mail.utoronto.ca>
2025-01-24 20:00:20 -0500

    Comments added to initial commit (html, js, readme)

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ggr472-ex1
  My website 
+> [!NOTE]
+* This note is created as part of excercise 2, step 4
+
+*This is for educational purposes only* 
+
+
--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const btn = document.getElementById("my-button"); 
+const btn = document.getElementById("my-button");
 // getting the button through the id named "my-button"
-btn.addEventListener("click", () => { 
+btn.addEventListener("click", () => {
     // adding a method for the object "button" to "fire" an "event",meaning so itdoes something. 
     // tere are 2 parameters: the name of the event - "click", 
     // and the function to call when the event happens.
-alert("You clicked me!"); 
-console.log("You clicked me!");
+    alert("You clicked me!");
+    console.log("You clicked me!");
+    // this is a comment as per excercise 2 step 4.
 });
